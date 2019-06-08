eba75c7b7ac22728ff4e4822fe2da8a1a1bda0ac
Tyler Sefcik <tyler.sefcik@gmail.com>
2019-06-08 06:55:32 -0400

    on to naive bayes

--- project/Algorithms.py
+++ project/Algorithms.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class Algorithms:
-
+    # Method used to train the winnow-2 algorithm
     def winnow2(self, data, predicted, theta, alpha, initial_weight):
         # Initialize classifier to number of columns and set initial values to initial_weight
         classifier = [initial_weight] * (data.shape[1])
@@ -36,6 +36,7 @@ class Algorithms:
         print(classifier)
         return classifier
 
+    # Method used to test the winnow-2 algorithm
     def winnow2_test(self, data, theta, classifier):
         # Initialize prediction classifier
         predict_classifier = []
@@ -56,44 +57,44 @@ class Algorithms:
         print(predict_classifier)
         return predict_classifier
 
+    # Method used to train the naive bayes classifier
     def naive_bayes_train(self, data, predicted):
-
+        # Get column list for output matrix
         col_list = list(data.columns)
         col_list.insert(0, "Overall Mean")
         output = pd.DataFrame(columns=[col_list])
         overall_mean = np.mean(predicted)
+        print("Overall Mean of classes")
         print(overall_mean)
+        print()
 
+        # Set initial values
         output.loc[0] = (1-overall_mean)
         output.loc[1] = (1 - overall_mean)
         output.loc[2] = overall_mean
         output.loc[3] = overall_mean
 
-        print(output)
-
+        # Split classes into true/false
         class_false = data[predicted == 0]
         class_true = data[predicted == 1]
-        print(class_false)
-        print(class_true)
 
+        # Add a middle point that will give us a balance point
         new_row = [.5] * (class_false.shape[1])
-
         class_false = class_false.append(pd.Series(new_row, index=class_false.columns), ignore_index=True)
         class_true = class_true.append(pd.Series(new_row, index=class_true.columns), ignore_index=True)
-        print(class_false)
-        print(class_true)
 
+        # Get the mean of each class
         class_false_means = class_false.mean()
         class_true_means = class_true.mean()
+        print("Means of each predicted class")
         print(class_false_means)
         print(class_true_means)
+        print()
 
         adjusted_false = 1 - class_false_means
-        print(adjusted_false)
-
         adjusted_true = 1 - class_true_means
-        print(adjusted_true)
 
+        # Set the output matrix with the means of the columns
         for col in col_list:
             if col != "Overall Mean":
                 output.loc[0, col] = adjusted_false[col]
@@ -101,46 +102,52 @@ class Algorithms:
                 output.loc[2, col] = adjusted_true[col]
                 output.loc[3, col] = class_true_means[col]
 
+        print("What will be used as our classifier for naive bayes")
         print(output)
+        print()
 
         return output
 
+    # Method used to test our naive bayes classifier
     def naive_bayes_test(self, data, mean_matrix):
         predictions = []
+        # Iterate through each row, and set initial output classifier
         for index, row in data.iterrows():
             class_false = [0] * (len(data.columns) + 1)
             class_false[0] = mean_matrix.iloc[0, 0]
             class_true = [0] * (len(data.columns) + 1)
             class_true[0] = mean_matrix.iloc[0, 0]
 
+            # Iterate through each column in the row
             for index2 in range(len(data.columns)):
+                # If the column is equal to 1, set the mean accordingly
                 if row[index2] == 1:
                     class_false[index2 + 1] = mean_matrix.iloc[1, index2 + 1]
                     class_true[index2 + 1] = mean_matrix.iloc[3, index2 + 1]
+                # If the column is equal to 0, set the mean accordingly
                 else:
                     class_false[index2 + 1] = mean_matrix.iloc[1, index2 + 1]
                     class_true[index2 + 1] = mean_matrix.iloc[3, index2 + 1]
 
+            # Take the product of each dataframe
             class_false_product = np.product(class_false)
             class_true_product = np.product(class_true)
 
-            print(class_false_product)
-            print(class_true_product)
-
+            # Compare to give us our prediction
             if class_false_product < class_true_product:
                 predictions.append(1)
             else:
                 predictions.append(0)
 
+        print("Predictions of classes")
         print(predictions)
-        print(len(predictions))
+        print()
+        return predictions
 
-
-
-
-
+    # Used to compare if a class was predicted correctly
     def compare_prediction(self, predict_classier, data):
         success = 0
+        # Iterate through predictions and see if there is a match
         for index in range(0, len(predict_classier) - 1):
 
             if predict_classier[index] == data.values[index]:
@@ -148,5 +155,5 @@ class Algorithms:
 
         success_rate = (success/data.shape[0]) * 100
         print("Success rate is: " + str(success_rate) + "%")
-
+        # Return the success rate
         return success_rate
--- project/Main.py
+++ project/Main.py
@@ -10,111 +10,273 @@ from project import Algorithms as alg
 
 def run_winnow_iris():
     success_rate = 0
-
-    for index in range(0, 50):
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
         iris_obj = iris.Iris()
         iris_data = iris_obj.setup_data_iris()
+        # Start algorithm
         winnow = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
         iris_data_train = iris_data.sample(frac=.667)
         iris_data_test = iris_data.drop(iris_data_train.index)
+        # Train classifier
         trainer_classifier = winnow.winnow2(iris_data_train.iloc[:, 0:4], iris_data_train["class"], 2, .5, 1)
+        # Test classifier
         test_classifier = winnow.winnow2_test(iris_data_test.iloc[:, 0:4], 2, trainer_classifier)
+        # Get success rate back
         success_rate = success_rate + winnow.compare_prediction(test_classifier, iris_data_test["class"])
-    success_rate = success_rate / 50
+    success_rate = success_rate / 25
     print("Average Winnow-2 success rate is: " + str(success_rate) + "%")
+    return success_rate
 
 
 def run_winnow_glass():
     success_rate = 0
-
-    for index in range(0, 50):
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
         glass_obj = glass.Glass()
         glass_data = glass_obj.setup_data_glass()
+        # Start algorithm
         winnow = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
         glass_data_train = glass_data.sample(frac=.667)
         glass_data_test = glass_data.drop(glass_data_train.index)
+        # Train classifier
         trainer_classifier = winnow.winnow2(glass_data_train.iloc[:, 0:9], glass_data_train["Type of glass"], 1, 10, 1)
+        # Test classifier
         test_classifier = winnow.winnow2_test(glass_data_test.iloc[:, 0:9], 2, trainer_classifier)
+        # Get success rate back
         success_rate = success_rate + winnow.compare_prediction(test_classifier, glass_data_test["Type of glass"])
-    success_rate = success_rate / 50
+    success_rate = success_rate / 25
     print("Average Winnow-2 success rate is: " + str(success_rate) + "%")
+    return success_rate
 
 
 def run_winnow_bc():
     success_rate = 0
-
-    for index in range(0, 50):
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
         bc_obj = bc.BreastCancer()
         bc_data = bc_obj.setup_data_breast_cancer()
+        # Start algorithm
         winnow = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
         bc_data_train = bc_data.sample(frac=.667)
         bc_data_test = bc_data.drop(bc_data_train.index)
+        # Train classifier
         trainer_classifier = winnow.winnow2(bc_data_train.iloc[:, 0:10], bc_data_train["Class"], 1, .5, 1)
+        # Test classifier
         test_classifier = winnow.winnow2_test(bc_data_test.iloc[:, 0:10], 2, trainer_classifier)
+        # Get success rate back
         success_rate = success_rate + winnow.compare_prediction(test_classifier, bc_data_test["Class"])
-    success_rate = success_rate / 50
+    success_rate = success_rate / 25
     print("Average Winnow-2 success rate is: " + str(success_rate) + "%")
+    return success_rate
 
 
 def run_winnow_soybean():
     success_rate = 0
-
-    for index in range(0, 50):
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
         soybean_obj = soy.Soybean()
         soybean_data = soybean_obj.setup_data_soybean()
+        # Start algorithm
         winnow = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
         soybean_data_train = soybean_data.sample(frac=.667)
         soybean_data_test = soybean_data.drop(soybean_data_train.index)
+        # Train classifier
         trainer_classifier = winnow.winnow2(soybean_data_train.iloc[:, 0:36], soybean_data_train["35"], 3, 10, 1)
+        # Test classifier
         test_classifier = winnow.winnow2_test(soybean_data_test.iloc[:, 0:36], 3, trainer_classifier)
+        # Get success rate back
         success_rate = success_rate + winnow.compare_prediction(test_classifier, soybean_data_test["35"])
-    success_rate = success_rate / 50
+    success_rate = success_rate / 25
     print("Average Winnow-2 success rate is: " + str(success_rate) + "%")
+    return success_rate
 
 
 def run_winnow_votes():
     success_rate = 0
-
-    for index in range(0, 50):
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
         votes_obj = votes.HouseVotes()
         votes_data = votes_obj.setup_data_votes()
+        # Start algorithm
         winnow = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
         votes_data_train = votes_data.sample(frac=.667)
         votes_data_test = votes_data.drop(votes_data_train.index)
+        # Train classifier
         trainer_classifier = winnow.winnow2(votes_data_train.iloc[:, 1:17], votes_data_train["class"], 3, 10, 1)
+        # Test classifier
         test_classifier = winnow.winnow2_test(votes_data_test.iloc[:, 1:17], 3, trainer_classifier)
+        # Get success rate back
         success_rate = success_rate + winnow.compare_prediction(test_classifier, votes_data_test["class"])
-    success_rate = success_rate / 50
+    success_rate = success_rate / 25
     print("Average Winnow-2 success rate is: " + str(success_rate) + "%")
+    return success_rate
 
 
 def run_naive_bayes_iris():
     success_rate = 0
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
+        iris_obj = iris.Iris()
+        iris_data = iris_obj.setup_data_iris()
+        # Start algorithm
+        naive = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
+        iris_data_train = iris_data.sample(frac=.667)
+        iris_data_test = iris_data.drop(iris_data_train.index)
+        # Train classifier
+        trainer_classifier = naive.naive_bayes_train(iris_data_train.iloc[:, 0:4], iris_data_train["class"])
+        # Test classifier
+        test_classifier = naive.naive_bayes_test(iris_data_test.iloc[:, 0:4], trainer_classifier)
+        # Get success rate back
+        success_rate = success_rate + naive.compare_prediction(test_classifier, iris_data_test["class"])
 
-    # for index in range(0, 50):
-    iris_obj = iris.Iris()
-    iris_data = iris_obj.setup_data_iris()
-    naive = alg.Algorithms()
-    iris_data_train = iris_data.sample(frac=.667)
-    iris_data_test = iris_data.drop(iris_data_train.index)
-    trainer_classifier = naive.naive_bayes_train(iris_data_train.iloc[:, 0:4], iris_data_train["class"])
-    test_classifier = naive.naive_bayes_test(iris_data_test.iloc[:, 0:4], trainer_classifier)
-    success_rate = naive.compare_prediction(test_classifier, iris_data_test["class"])
-
-#   success_rate = success_rate / 50
+    success_rate = success_rate / 25
     print("Average Naive Bayes success rate is: " + str(success_rate) + "%")
+    return success_rate
+
+
+def run_naive_bayes_glass():
+    success_rate = 0
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
+        glass_obj = glass.Glass()
+        glass_data = glass_obj.setup_data_glass()
+        # Start algorithm
+        naive = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
+        glass_data_train = glass_data.sample(frac=.667)
+        glass_data_test = glass_data.drop(glass_data_train.index)
+        # Train classifier
+        trainer_classifier = naive.naive_bayes_train(glass_data_train.iloc[:, 0:9], glass_data_train["Type of glass"])
+        # Test classifier
+        test_classifier = naive.naive_bayes_test(glass_data_test.iloc[:, 0:9], trainer_classifier)
+        # Get success rate back
+        success_rate = success_rate + naive.compare_prediction(test_classifier, glass_data_test["Type of glass"])
+
+    success_rate = success_rate / 25
+    print("Average Naive Bayes success rate is: " + str(success_rate) + "%")
+    return success_rate
+
+
+def run_naive_bayes_bc():
+    success_rate = 0
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
+        bc_obj = bc.BreastCancer()
+        bc_data = bc_obj.setup_data_breast_cancer()
+        # Start algorithm
+        naive = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
+        bc_data_train = bc_data.sample(frac=.667)
+        bc_data_test = bc_data.drop(bc_data_train.index)
+        # Train classifier
+        trainer_classifier = naive.naive_bayes_train(bc_data_train.iloc[:, 0:10], bc_data_train["Class"])
+        # Test classifier
+        test_classifier = naive.naive_bayes_test(bc_data_test.iloc[:, 0:10], trainer_classifier)
+        # Get success rate back
+        success_rate = success_rate + naive.compare_prediction(test_classifier, bc_data_test["Class"])
+
+    success_rate = success_rate / 25
+    print("Average Naive Bayes success rate is: " + str(success_rate) + "%")
+    return success_rate
+
+
+def run_naive_bayes_soybean():
+    success_rate = 0
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
+        soybean_obj = soy.Soybean()
+        soybean_data = soybean_obj.setup_data_soybean()
+        # Start algorithm
+        naive = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
+        soybean_data_train = soybean_data.sample(frac=.667)
+        soybean_data_test = soybean_data.drop(soybean_data_train.index)
+        # Train classifier
+        trainer_classifier = naive.naive_bayes_train(soybean_data_train.iloc[:, 0:36], soybean_data_train["35"])
+        # Test classifier
+        test_classifier = naive.naive_bayes_test(soybean_data_test.iloc[:, 0:36], trainer_classifier)
+        # Get success rate back
+        success_rate = success_rate + naive.compare_prediction(test_classifier, soybean_data_test["35"])
+
+    success_rate = success_rate / 25
+    print("Average Naive Bayes success rate is: " + str(success_rate) + "%")
+    return success_rate
+
+
+def run_naive_bayes_votes():
+    success_rate = 0
+    # Provide 25 iterations
+    for index in range(0, 25):
+        # Setup data
+        votes_obj = votes.HouseVotes()
+        votes_data = votes_obj.setup_data_votes()
+        # Start algorithm
+        naive = alg.Algorithms()
+        # Split the data set into 2/3 and 1/3
+        votes_data_train = votes_data.sample(frac=.667)
+        votes_data_test = votes_data.drop(votes_data_train.index)
+        # Train classifier
+        trainer_classifier = naive.naive_bayes_train(votes_data_train.iloc[:, 1:17], votes_data_train["class"])
+        # Test classifier
+        test_classifier = naive.naive_bayes_test(votes_data_test.iloc[:, 1:17], trainer_classifier)
+        # Get success rate back
+        success_rate = success_rate + naive.compare_prediction(test_classifier, votes_data_test["class"])
+
+    success_rate = success_rate / 25
+    print("Average Naive Bayes success rate is: " + str(success_rate) + "%")
+    return success_rate
 
 
 def main():
     # Winnow-2
-    # run_winnow_iris()
-    # run_winnow_glass()
-    # run_winnow_bc()
-    # run_winnow_soybean()
-    # run_winnow_votes()
+    winnow_iris = run_winnow_iris()
+    winnow_glass = run_winnow_glass()
+    winnow_bc = run_winnow_bc()
+    winnow_soy = run_winnow_soybean()
+    winnow_votes = run_winnow_votes()
+
+    average_winnow_combined = (winnow_iris + winnow_glass + winnow_bc + winnow_soy + winnow_votes) / 5
 
     # Naive Bayes
-    run_naive_bayes_iris()
+    naive_iris = run_naive_bayes_iris()
+    naive_glass = run_naive_bayes_glass()
+    naive_bc = run_naive_bayes_bc()
+    naive_soy = run_naive_bayes_soybean()
+    naive_votes = run_naive_bayes_votes()
+
+    average_naive_combined = (naive_iris + naive_glass + naive_bc + naive_soy + naive_votes) / 5
+
+    print("Overall statistics")
+    print()
+    print("Winnow-2 iris: " + str(winnow_iris) + "%")
+    print("Winnow-2 glass: " + str(winnow_glass) + "%")
+    print("Winnow-2 breast cancer: " + str(winnow_bc) + "%")
+    print("Winnow-2 soybean: " + str(winnow_soy) + "%")
+    print("Winnow-2 votes: " + str(winnow_votes) + "%")
+    print("Naive Bayes iris: " + str(naive_iris) + "%")
+    print("Naive Bayes glass: " + str(naive_glass) + "%")
+    print("Naive Bayes breast cancer: " + str(naive_bc) + "%")
+    print("Naive Bayes soybean: " + str(naive_soy) + "%")
+    print("Naive Bayes votes: " + str(naive_votes) + "%")
+    print()
+    print("Average Winnow-2 Accuracy: " + str(average_winnow_combined) + "%")
+    print("Average Naive Bayes Accuracy: " + str(average_naive_combined) + "%")
 
 
 if __name__ == "__main__":
